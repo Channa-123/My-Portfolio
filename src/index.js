fd43f497c42328c688dd9a97618332f89002ea4a
Channa-123 <96125389+Channa-123@users.noreply.github.com>
2025-11-16 00:49:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import emailjs from '@emailjs/browser';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import AOS from 'aos'; // Add this
+import 'aos/dist/aos.css';
+
+emailjs.init({
+  publicKey: 'l8aPEcFIgTJfvmyo8', // Replace with your EmailJS Public Key
+});
+
+AOS.init({
+  duration: 800, // Animation duration in ms
+  once: true, // Animate only once
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +21,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
