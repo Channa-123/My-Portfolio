f7decc1494a20df305ac5ead195bf0e45aa2d05a
Channa-123 <96125389+Channa-123@users.noreply.github.com>
2025-11-17 00:46:59 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2025 CHANNA PRASAD. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
+
